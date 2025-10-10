1607db3e71c2d5e7f9c41cab645e753634749ff0
Danna Collazos <ddcollazos@miuandes.cl>
2025-10-10 19:55:14 -0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :users, only: [ :index, :show ]
+  resources :progress_entries, only: [ :index, :show ]
+  resources :challenge_participations, only: [ :index, :show ]
+  resources :bagdes, only: [ :index, :show ]
   resources :challenges, only: [ :index, :show ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
