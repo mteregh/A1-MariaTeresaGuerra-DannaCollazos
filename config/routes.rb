da0455bc83b3fa652d602f839363bd70f5298f4b
mteregh <mtguerra@miuandes.cl>
2025-10-26 15:40:11 -0300

    Arreglos seed y agregar navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,13 @@
 Rails.application.routes.draw do
-  resources :users, only: [ :index, :show ]
-  resources :progress_entries, only: [ :index, :show ]
-  resources :challenge_participations, only: [ :index, :show ]
-  resources :badges, only: [ :index, :show ]
-  resources :challenges, only: [ :index, :show ]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show]
+  resources :progress_entries, only: [:index, :show]
+  resources :challenge_participations, only: [:index, :show]
+  resources :badges, only: [:index, :show]
+  resources :challenges, only: [:index, :show]
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Health check
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Root path
+  root "users#index"
 end
